4d0ffb080e99976d9be5cf3446bf5bc5396a464c
Mulishova Ekaterina <mulishova.e@yandex.ru>
2020-05-03 17:23:15 +0300

    added textureAtlas

--- core/src/com/mygdx/game/Stone.java
+++ core/src/com/mygdx/game/Stone.java
@@ -2,11 +2,12 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Stone {
-    private Texture texture;
+    private TextureRegion texture;
     private Vector2 position;
     private Rectangle rectangle; // для проверки столкновений
 
@@ -21,7 +22,7 @@ public class Stone {
         return rectangle;
     }
 
-    public Stone (Texture texture, Vector2 position) {
+    public Stone (TextureRegion texture, Vector2 position) {
         this.texture = texture;
         this.position = position;
         this.rectangle = new Rectangle(position.x, position.y, WIDTH, HEIGHT);
